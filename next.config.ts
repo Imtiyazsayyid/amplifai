1a92ab4ec6b370c013ad9feffb04b74ac7d5261e
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2025-03-04 12:43:53 +0530

    Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // serverExternalPackages: ["@sparticuz/chromium"],
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+      {
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        hostname: "img.clerk.com",
+      },
+      {
+        hostname: "developers.elementor.com",
+      },
+      {
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
-
 export default nextConfig;
